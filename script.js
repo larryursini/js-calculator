723a023722001db0c20d4a070deee85fd8066284
larryursini <39272661+larryursini@users.noreply.github.com>
2023-03-27 18:10:41 -0400

    its working as expected

--- script.js
+++ script.js
@@ -23,26 +23,32 @@ clear function as needed
 */
 let screenDisplay = document.getElementById("calc-display") 
 let clear = true
+let operandOne = ""
 
 $(document).ready(function(){
     $("button").click(function(event){
-        //let operandOne = ""
+    
         if (this.id === "reset"){
             screenDisplay.innerText = ""
-        }else if (this.id !== "=" && clear === true) {
+        }else if (this.id !== "=" && clear === true && this.className !== "operation-button") {
             console.log(this.id)
+            console.log(this.className)
             screenDisplay.innerText += this.id
-        }else if (this.id !== "=" && clear === false) {
+        }else if (this.id !== "=" && clear === false && this.className !== "operation-button") {
             screenDisplay.innerText = ""
             console.log(this.id)
             screenDisplay.innerText += this.id
             clear = true
-        }else if (this.id !== "=") {
-            console.log(this.id)
-            screenDisplay.innerText += this.id
+        }else if (this.className === "operation-button") {
+            operandOne = screenDisplay.innerText
+            operandOne += this.id
+            console.log(`operator ${operandOne}`)
+            screenDisplay.innerText = ""
         }else if (this.id === "=" ) {
-            screenDisplay.innerText = eval
-            (screenDisplay.innerText)
+            operandOne += screenDisplay.innerText
+            operandOne = eval(operandOne)
+            console.log(`this is ${operandOne}`)
+            screenDisplay.innerText = operandOne
             clear = false
         }
 
